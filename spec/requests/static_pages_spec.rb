6faae447e04e4391e1863795087403238ce3936d
Y Kang <yokang38@gmail.com>
2013-08-31 17:18:03 -0700

    Chapter 3 Exercise 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static pages" do
     
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -24,7 +24,7 @@ describe "Static pages" do
     
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
    
@@ -36,7 +36,7 @@ describe "Static pages" do
     
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
   
@@ -48,7 +48,7 @@ describe "Static pages" do
     
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
